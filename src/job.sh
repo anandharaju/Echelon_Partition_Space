3812c3d4dfd887ae59ee68957a80095ddc8595ad
aduraira <anandharajuds@gmail.com>
2020-05-19 00:39:49 -0700

    Compute Canada Job changes

--- src/job.sh
+++ src/job.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --account=def-wangk
-#SBATCH --time=12:00:00
-#SBATCH --job-name=G_DS1_0_16G
+#SBATCH --time=18:00:00
+#SBATCH --job-name=space_5
 
 #SBATCH --cpus-per-task=8
 #SBATCH --cores-per-socket=8
 #SBATCH --gres=gpu:p100:1
-#SBATCH --mem=16G
+#SBATCH --mem=24G
 
 #SBATCH -o /home/aduraira/projects/def-wangk/aduraira/cc_out/job%j.out
 #SBATCH -e /home/aduraira/projects/def-wangk/aduraira/cc_out/job%j.err
